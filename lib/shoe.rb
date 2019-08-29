0c9179c8633caabd946bef24aad634e24b17b714
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-29 22:46:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,13 @@
 class Shoe
   
+  attr_reader :brand 
+  attr_accessor :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end 
+  
+  def cobble
+    puts ""
   
 end 
